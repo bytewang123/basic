a380447554acf21d2a6b9a590d3a197ac81233bf
bytewang123 <740948792@qq.com>
2020-01-12 16:39:20 +0800

    add official solution

--- mergeLinkedList.py
+++ mergeLinkedList.py
@@ -55,7 +55,18 @@ def mergeTwoLists(l1, l2):
 
         return newL               
 
-        
+def officialMerged(l1, l2):
+    
+    if l1 is None:
+        return l2
+    elif l2 is None:
+        return l1
+    elif l1.val < l2.val:
+        l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1
+    else:
+        l2.next = self.mergeTwoLists(l1, l2.next)
+        return l2
 
 
 l1 = ListNode(1)
